0fbe0101337804059643d9be9b5d1a7fed79f332
DacioSB <dacio.bezerra@ccc.ufcg.edu.br>
2024-05-27 20:15:20 -0300

    :construction: create sidebar component

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,3 +1,5 @@
+import Sidebar from '@/components/Sidebar';
+import Navbar from '@/components/ui/Navbar';
 import React from 'react'
 
 function HomeLayout({ children }: {
@@ -5,9 +7,9 @@ function HomeLayout({ children }: {
 }) {
     return (
         <main className='relative'>
-            Navbar
+            <Navbar />
             <div className='flex'>
-                Sidebar
+                <Sidebar />
                 <section className='flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14'>
                     <div className='w-full'>{children}</div>
                 </section>
