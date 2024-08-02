d334410fb61b52ebc2cfeb512323da72145f4ffd
DacioSB <dacio.bezerra@ccc.ufcg.edu.br>
2024-08-02 16:42:14 -0300

    :sparkles: create homecard list

---
+++ components/MeetingTypeList.tsx
@@ -0,0 +1,42 @@
+/* eslint-disable camelcase */
+'use client';
+
+import HomeCard from './HomeCard';
+
+import React from 'react'
+
+export const MeetingTypeList = () => {
+  return (
+    <section className='grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-4'>
+        <HomeCard
+        img="/icons/add-meeting.svg"
+        title="New Meeting"
+        description="Start an instant meeting"
+        //handleClick={() => setMeetingState('isInstantMeeting')}
+      />
+      <HomeCard
+        img="/icons/join-meeting.svg"
+        title="Join Meeting"
+        description="via invitation link"
+        className="bg-blue-1"
+        //handleClick={() => setMeetingState('isJoiningMeeting')}
+      />
+      <HomeCard
+        img="/icons/schedule.svg"
+        title="Schedule Meeting"
+        description="Plan your meeting"
+        className="bg-purple-1"
+        //handleClick={() => setMeetingState('isScheduleMeeting')}
+      />
+      <HomeCard
+        img="/icons/recordings.svg"
+        title="View Recordings"
+        description="Meeting Recordings"
+        className="bg-yellow-1"
+        //handleClick={() => router.push('/recordings')}
+      />
+    </section>
+  )
+}
+
+
