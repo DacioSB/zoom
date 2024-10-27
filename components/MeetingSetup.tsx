97587a784d5aa5d6a16c3077ac8ff8b999b83b88
DacioSB <dacio.bezerra@ccc.ufcg.edu.br>
2024-10-27 19:45:17 -0300

    :children_crossing: create generic card component

---
+++ components/MeetingSetup.tsx
@@ -0,0 +1,11 @@
+'use client';
+import { useEffect, useState } from 'react';
+import {
+  DeviceSettings,
+  VideoPreview,
+  useCall,
+  useCallStateHooks,
+} from '@stream-io/video-react-sdk';
+
+import Alert from './Alert';
+import { Button } from './ui/button';
