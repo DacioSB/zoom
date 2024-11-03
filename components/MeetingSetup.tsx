4885e995d0d47c03c8ecd8a22cede90057cd0075
DacioSB <dacio.bezerra@ccc.ufcg.edu.br>
2024-11-03 20:29:51 -0300

    :necktie: update logic of meetingsetup and call mic and camera toggle

--- components/MeetingSetup.tsx
+++ components/MeetingSetup.tsx
@@ -9,3 +9,38 @@ import {
 
 import Alert from './Alert';
 import { Button } from './ui/button';
+
+const MeetingSetup = ({
+  setIsSetupComplete,
+}: {
+  setIsSetupComplete: (value: boolean) => void;
+}) => {
+  // https://getstream.io/video/docs/react/guides/call-and-participant-state/#call-state
+  const { useCallEndedAt, useCallStartsAt } = useCallStateHooks();
+  const callStartsAt = useCallStartsAt();
+  const callEndedAt = useCallEndedAt();
+  const callTimeNotArrived =
+    callStartsAt && new Date(callStartsAt) > new Date();
+  const callHasEnded = !!callEndedAt;
+
+  const call = useCall();
+
+  if (!call) {
+    throw new Error(
+      'useStreamCall must be used within a StreamCall component.',
+    );
+  }
+  const [isMicCamToggled, setIsMicCamToggled] = useState(false);
+
+  useEffect(() => {
+    if (isMicCamToggled) {
+      call.camera.disable();
+      call.microphone.disable();
+    } else {
+      call.camera.enable();
+      call.microphone.enable();
+    }
+  }, [isMicCamToggled, call.camera, call.microphone]);
+}
+
+export default MeetingSetup;
