6a51e6fa8e0f90afb7540c20c3869babb0ca6d92
dacio.bezerra <dacio.bezerra@dataprev.gov.br>
2024-11-19 15:39:10 -0300

    :lipstick: create meeting room component and dependencies

---
+++ components/MeetingRoom.tsx
@@ -0,0 +1,26 @@
+'use client';
+import { useState } from 'react';
+import {
+  CallControls,
+  CallParticipantsList,
+  CallStatsButton,
+  CallingState,
+  PaginatedGridLayout,
+  SpeakerLayout,
+  useCallStateHooks,
+} from '@stream-io/video-react-sdk';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { Users, LayoutList } from 'lucide-react';
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu';
+import Loader from './Loader';
+import EndCallButton from './EndCallButton';
+import { cn } from '@/lib/utils';
+
+type CallLayoutType = 'grid' | 'speaker-left' | 'speaker-right';
