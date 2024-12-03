8fc316602faf0393e2d6296cf9d964d524dc8860
dacio.bezerra <dacio.bezerra@dataprev.gov.br>
2024-12-03 15:43:45 -0300

    :sparkles: create meetingroom component

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,4 +1,16 @@
 import React from 'react'
+'use client';
+
+import { useState } from 'react';
+import { useUser } from '@clerk/nextjs';
+import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk';
+import { useParams } from 'next/navigation';
+import { Loader } from 'lucide-react';
+
+import { useGetCallById } from '@/hooks/useGetCallById';
+import Alert from '@/components/Alert';
+import MeetingSetup from '@/components/MeetingSetup';
+import MeetingRoom from '@/components/MeetingRoom';
 
 const Meeting = ({ params }: { params: { id: string } }) => {
     return (
